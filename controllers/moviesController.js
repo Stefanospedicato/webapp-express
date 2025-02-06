6f755273e31e78b0c07f1d310fff309068217ff6
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-02-06 13:28:12 +0100

    feat: add index route

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,8 +1,17 @@
+const connection = require('../data/moviesDB')
+
 const index = (req,res) => {
-  res.send('Lista movies')
+  
+  const sql = 'SELECT * FROM movies'
+
+  connection.query(sql, (err, results) => {
+    if(err) return res.status(500).json({err:'Connesione fallita'})
+    res.json(results)
+  })
+
 }
 
-const show = (req,re) => {
+const show = (req,res) => {
   const id = req.params.id
   res.send(`Mostro il movie con id= ${id}`)
 }
